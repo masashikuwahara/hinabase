3fcaee87c930cf26760ac3dc84d2c9aad586844f
masashikuwahara <brucemclarenp4@gmail.com>
2025-01-28 00:21:29 +0900

    Modelを実装

---
+++ app/Models/Song.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'release_date'];
+
+    public function members()
+    {
+        return $this->belongsToMany(Member::class, 'song_members');
+    }
+}
