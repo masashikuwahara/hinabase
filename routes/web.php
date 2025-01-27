c6bac76b4f6d07b15bb162d6517603082eede7dd
masashikuwahara <brucemclarenp4@gmail.com>
2025-01-28 00:54:21 +0900

    songsページ追加

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SongController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/songs', [SongController::class, 'index'])->name('songs.index');
