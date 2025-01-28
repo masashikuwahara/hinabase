12d71e51227dff4ebb0640878d1fdee09d574248
masashikuwahara <brucemclarenp4@gmail.com>
2025-01-29 01:25:50 +0900

    ルーター追加

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SongController;
+use App\Http\Controllers\MemberController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/songs', [SongController::class, 'index'])->name('songs.index');
+Route::get('/members', [MemberController::class, 'index'])->name('members.index'); // メンバー一覧
+Route::get('/members/{id}', [MemberController::class, 'show'])->name('members.show'); // プロフィール詳細
