9da6d085c3422510b4be06d4feaa8e4bb9d3467f
masashikuwahara <brucemclarenp4@gmail.com>
2025-01-30 00:33:28 +0900

    song controller修正

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -7,11 +7,17 @@ use Illuminate\Http\Request;
 
 class SongController extends Controller
 {
+    // 楽曲一覧ページ
     public function index()
     {
-        // 楽曲ごとに参加メンバーを取得
-        $songs = Song::with('members')->get();
-
+        $songs = Song::all(); // 楽曲データを取得
         return view('songs.index', compact('songs'));
+    }
+
+    // 楽曲詳細ページ
+    public function show($id)
+    {
+        $song = Song::with('members')->findOrFail($id); // 楽曲の参加メンバーを取得
+        return view('songs.show', compact('song'));
     }
 }
