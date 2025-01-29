ae4b6a419cf99e7d147d04566c5c1f5a372aaefc
masashikuwahara <brucemclarenp4@gmail.com>
2025-01-30 00:34:27 +0900

    ルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,6 @@ Route::get('/', function () {
 });
 
 Route::get('/songs', [SongController::class, 'index'])->name('songs.index');
+Route::get('/songs/{id}', [SongController::class, 'show'])->name('songs.show'); // 楽曲詳細
 Route::get('/members', [MemberController::class, 'index'])->name('members.index'); // メンバー一覧
 Route::get('/members/{id}', [MemberController::class, 'show'])->name('members.show'); // プロフィール詳細
