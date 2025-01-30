fe42ee87e025cba5b36f4218953efe97376c71f9
masashikuwahara <brucemclarenp4@gmail.com>
2025-01-31 00:28:44 +0900

    ルーティング修正

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,10 @@ use App\Http\Controllers\SongController;
 use App\Http\Controllers\MemberController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::get('/songs', [SongController::class, 'index'])->name('songs.index');
+Route::get('/songs', [SongController::class, 'index'])->name('songs.index'); // 楽曲一覧
 Route::get('/songs/{id}', [SongController::class, 'show'])->name('songs.show'); // 楽曲詳細
 Route::get('/members', [MemberController::class, 'index'])->name('members.index'); // メンバー一覧
 Route::get('/members/{id}', [MemberController::class, 'show'])->name('members.show'); // プロフィール詳細
