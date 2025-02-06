7365ace13cc864a182c9e32e082b1d3a09b9ebd4
masashikuwahara <brucemclarenp4@gmail.com>
2025-02-07 00:11:39 +0900

    seeder変更

--- database/seeders/SongMemberSeeder.php
+++ database/seeders/SongMemberSeeder.php
@@ -12,15 +12,22 @@ class SongMemberSeeder extends Seeder
     public function run()
     {
         $songA = Song::where('title', '卒業写真だけが知ってる')->first();
+        $songB = Song::where('title', '足の小指を箪笥の角にぶつけた')->first();
 
-        $memberA = Member::where('name', '金村美玖')->first();
+        $memberA = Member::where('name', '小坂菜緒')->first();
+        $memberB = Member::where('name', '山下葉留花')->first();
+        $memberC = Member::where('name', '小西夏菜実')->first();
 
         // 曲Aにメンバーを追加（メンバーAをセンターに設定）
         $songA->members()->attach([
-            $memberA->id => ['is_center' => false]
+            $memberA->id => ['is_center' => true],
+            $memberB->id => ['is_center' => false],
         ]);
         // 曲Bにメンバーを追加（メンバーCをセンターに設定）
-
+        $songB->members()->attach([
+            $memberB->id => ['is_center' => false],
+            $memberC->id => ['is_center' => true],
+        ]);
         
     }
 }
