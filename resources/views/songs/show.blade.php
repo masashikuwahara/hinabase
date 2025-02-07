7aeea950cc37f43029d1f49103a92cb3ec906655
masashikuwahara <brucemclarenp4@gmail.com>
2025-02-08 03:19:20 +0900

    楽曲詳細ページ修正

--- resources/views/songs/show.blade.php
+++ resources/views/songs/show.blade.php
@@ -4,18 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $song->title }} </title>
+    <style>
+    @media screen and (max-width:400px) {
+        .video iframe {
+            position: relative;
+            width: 100%;
+            padding-top: 56.25%; /* 16:9のアスペクト比 */
+            height: 0;
+            top: 0;
+            left: 0;
+        }
+    }
+    </style>
 </head>
 <body>
   <h1>{{ $song->title }} の詳細</h1>
 
-<h2>参加メンバー</h2>
 @if ($song->members->isEmpty())
     <p>この楽曲にはまだ参加メンバーが登録されていません。</p>
 @else
+    <img src="{{ asset('storage/' . $song->photo) }}" alt="ジャケット" width="150"><br>
     リリース日: {{ $song->release }}
     作詞: {{ $song->lyricist }}
     作曲: {{ $song->composer }}
     編曲: {{ $song->arranger }}
+    <h2>参加メンバー</h2>
     <ul>
         @foreach ($song->members as $member)
             <li>
@@ -26,6 +39,9 @@
             </li>
         @endforeach
     </ul>
+    <div class="video">
+        {!! $song->youtube !!}
+    </div>
 @endif
 <a href="{{ route('songs.index') }}">楽曲一覧へ戻る</a>
 </body>
