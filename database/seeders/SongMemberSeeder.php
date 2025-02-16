1b3b04f0c89025376c80cd81e7d7f8e20b20c3fe
masashikuwahara <brucemclarenp4@gmail.com>
2025-02-17 00:30:09 +0900

    Seeder修正

--- database/seeders/SongMemberSeeder.php
+++ database/seeders/SongMemberSeeder.php
@@ -11,31 +11,36 @@ class SongMemberSeeder extends Seeder
 {
     public function run()
     {
-        $songA = Song::where('title', 'それでも歩いてる')->first();
-        $songB = Song::where('title', 'NO WAR in the future')->first();
-        $songC = Song::where('title', 'イマニミテイロ')->first();
-        $songD = Song::where('title', '半分の記憶')->first();
+        $songA = Song::where('title', '僕なんか')->first();
+        $songB = Song::where('title', '飛行機雲ができる理由')->first();
+        $songC = Song::where('title', 'もうこんなに好きになれない')->first();
+        $songD = Song::where('title', 'ゴーフルと君')->first();
+        $songE = Song::where('title', '真夜中の懺悔大会')->first();
+        $songF = Song::where('title', '恋した魚は空を飛ぶ')->first();
+        $songG = Song::where('title', '知らないうちに愛されていた')->first();
 
-        $memberA = Member::where('name', '井口眞緒')->first();
-        $memberB = Member::where('name', '潮紗理菜')->first();
-        $memberC = Member::where('name', '柿崎芽実')->first();
-        $memberD = Member::where('name', '影山優佳')->first();
-        $memberE = Member::where('name', '加藤史帆')->first();
-        $memberF = Member::where('name', '齊藤京子')->first();
-        $memberG = Member::where('name', '佐々木久美')->first();
-        $memberH = Member::where('name', '佐々木美玲')->first();
-        $memberI = Member::where('name', '高瀬愛奈')->first();
-        $memberJ = Member::where('name', '高本彩花')->first();
-        $memberK = Member::where('name', '東村芽依')->first();
-        $memberL = Member::where('name', '金村美玖')->first();
-        $memberM = Member::where('name', '河田陽菜')->first();
-        $memberN = Member::where('name', '小坂菜緒')->first();
-        $memberO = Member::where('name', '富田鈴花')->first();
-        $memberP = Member::where('name', '丹生明里')->first();
-        $memberQ = Member::where('name', '濱岸ひより')->first();
-        $memberR = Member::where('name', '松田好花')->first();
-        $memberS = Member::where('name', '宮田愛萌')->first();
-        $memberT = Member::where('name', '渡邉美穂')->first();
+        $memberA = Member::where('name', '潮紗理菜')->first();
+        $memberB = Member::where('name', '影山優佳')->first();
+        $memberC = Member::where('name', '加藤史帆')->first();
+        $memberD = Member::where('name', '齊藤京子')->first();
+        $memberE = Member::where('name', '佐々木久美')->first();
+        $memberF = Member::where('name', '佐々木美玲')->first();
+        $memberG = Member::where('name', '高瀬愛奈')->first();
+        $memberH = Member::where('name', '高本彩花')->first();
+        $memberI = Member::where('name', '東村芽依')->first();
+        $memberJ = Member::where('name', '金村美玖')->first();
+        $memberK = Member::where('name', '河田陽菜')->first();
+        $memberL = Member::where('name', '小坂菜緒')->first();
+        $memberM = Member::where('name', '富田鈴花')->first();
+        $memberN = Member::where('name', '丹生明里')->first();
+        $memberO = Member::where('name', '濱岸ひより')->first();
+        $memberP = Member::where('name', '松田好花')->first();
+        $memberQ = Member::where('name', '宮田愛萌')->first();
+        $memberR = Member::where('name', '渡邉美穂')->first();
+        $memberS = Member::where('name', '上村ひなの')->first();
+        $memberT = Member::where('name', '髙橋未来虹')->first();
+        $memberU = Member::where('name', '森本茉莉')->first();
+        $memberV = Member::where('name', '山口陽世')->first();
 
         $songA->members()->attach([
             $memberA->id => ['is_center' => false],
@@ -43,13 +48,13 @@ class SongMemberSeeder extends Seeder
             $memberC->id => ['is_center' => false],
             $memberD->id => ['is_center' => false],
             $memberE->id => ['is_center' => false],
-            $memberF->id => ['is_center' => true],
+            $memberF->id => ['is_center' => false],
             $memberG->id => ['is_center' => false],
             $memberH->id => ['is_center' => false],
             $memberI->id => ['is_center' => false],
             $memberJ->id => ['is_center' => false],
             $memberK->id => ['is_center' => false],
-            $memberL->id => ['is_center' => false],
+            $memberL->id => ['is_center' => true],
             $memberM->id => ['is_center' => false],
             $memberN->id => ['is_center' => false],
             $memberO->id => ['is_center' => false],
@@ -58,6 +63,8 @@ class SongMemberSeeder extends Seeder
             $memberR->id => ['is_center' => false],
             $memberS->id => ['is_center' => false],
             $memberT->id => ['is_center' => false],
+            $memberU->id => ['is_center' => false],
+            $memberV->id => ['is_center' => false],
         ]);
         $songB->members()->attach([
             $memberA->id => ['is_center' => false],
@@ -65,13 +72,13 @@ class SongMemberSeeder extends Seeder
             $memberC->id => ['is_center' => false],
             $memberD->id => ['is_center' => false],
             $memberE->id => ['is_center' => false],
-            $memberF->id => ['is_center' => true],
+            $memberF->id => ['is_center' => false],
             $memberG->id => ['is_center' => false],
             $memberH->id => ['is_center' => false],
             $memberI->id => ['is_center' => false],
             $memberJ->id => ['is_center' => false],
             $memberK->id => ['is_center' => false],
-            $memberL->id => ['is_center' => false],
+            $memberL->id => ['is_center' => true],
             $memberM->id => ['is_center' => false],
             $memberN->id => ['is_center' => false],
             $memberO->id => ['is_center' => false],
@@ -80,17 +87,32 @@ class SongMemberSeeder extends Seeder
             $memberR->id => ['is_center' => false],
             $memberS->id => ['is_center' => false],
             $memberT->id => ['is_center' => false],
+            $memberU->id => ['is_center' => false],
+            $memberV->id => ['is_center' => false],
         ]);
         $songC->members()->attach([
-            $memberA->id => ['is_center' => false],
+            $memberJ->id => ['is_center' => false],
+            $memberL->id => ['is_center' => false],
+            $memberO->id => ['is_center' => false],
+        ]);
+        $songD->members()->attach([
+            $memberS->id => ['is_center' => false],
+            $memberT->id => ['is_center' => false],
+            $memberU->id => ['is_center' => false],
+            $memberV->id => ['is_center' => true],
+        ]);
+        $songE->members()->attach([
+            $memberA->id => ['is_center' => true],
             $memberB->id => ['is_center' => false],
             $memberC->id => ['is_center' => false],
             $memberD->id => ['is_center' => false],
             $memberE->id => ['is_center' => false],
             $memberF->id => ['is_center' => false],
             $memberG->id => ['is_center' => false],
-            $memberH->id => ['is_center' => true],
+            $memberH->id => ['is_center' => false],
             $memberI->id => ['is_center' => false],
+        ]);
+        $songF->members()->attach([
             $memberJ->id => ['is_center' => false],
             $memberK->id => ['is_center' => false],
             $memberL->id => ['is_center' => false],
@@ -99,11 +121,9 @@ class SongMemberSeeder extends Seeder
             $memberO->id => ['is_center' => false],
             $memberP->id => ['is_center' => false],
             $memberQ->id => ['is_center' => false],
-            $memberR->id => ['is_center' => false],
-            $memberS->id => ['is_center' => false],
-            $memberT->id => ['is_center' => false],
+            $memberR->id => ['is_center' => true],
         ]);
-        $songD->members()->attach([
+        $songG->members()->attach([
             $memberA->id => ['is_center' => false],
             $memberB->id => ['is_center' => false],
             $memberC->id => ['is_center' => false],
@@ -117,13 +137,15 @@ class SongMemberSeeder extends Seeder
             $memberK->id => ['is_center' => false],
             $memberL->id => ['is_center' => false],
             $memberM->id => ['is_center' => false],
-            $memberN->id => ['is_center' => true],
+            $memberN->id => ['is_center' => false],
             $memberO->id => ['is_center' => false],
             $memberP->id => ['is_center' => false],
             $memberQ->id => ['is_center' => false],
             $memberR->id => ['is_center' => false],
             $memberS->id => ['is_center' => false],
             $memberT->id => ['is_center' => false],
+            $memberU->id => ['is_center' => false],
+            $memberV->id => ['is_center' => false],
         ]);
     }
 }
--- database/seeders/SongSeeder.php
+++ database/seeders/SongSeeder.php
@@ -11,70 +11,15 @@ class SongSeeder extends Seeder
     public function run()
     {
         Song::create([
-            'title' => 'キュン',
-            'release' => '2019-03-27',
+            'title' => '僕なんか',
+            'release' => '2022-06-01',
             'lyricist' => '秋元康',
-            'composer' => '野村陽一郎',
-            'arranger' => '野村陽一郎',
-            'is_recorded' => 'キュン',
+            'composer' => '温詞',
+            'arranger' => '温詞、TomoLow',
+            'is_recorded' => '僕なんか',
             'titlesong' => '1',
-            'youtube' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/K5HPhoqyO4U?si=-Ui4lIU3jxyY1aEN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
-            'photo' => 'photos/kyun.jpg',
-        ]);
-        Song::create([
-            'title' => 'JOYFUL LOVE',
-            'release' => '2019-03-27',
-            'lyricist' => '秋元康',
-            'composer' => '前迫潤哉、Dr.Lilcom',
-            'arranger' => 'Dr.Lilcom',
-            'is_recorded' => 'キュン',
-            'titlesong' => '0',
-            'youtube' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/mbXtz9zGB_E?si=w6VW1WkyvD6IhaOU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
-            'photo' => 'photos/kyun.jpg',
-        ]);
-        Song::create([
-            'title' => '耳に落ちる涙',
-            'release' => '2019-03-27',
-            'lyricist' => '秋元康',
-            'composer' => '西井昌明',
-            'arranger' => '若田部誠',
-            'is_recorded' => 'キュン',
-            'titlesong' => '0',
-            'youtube' => '-',
-            'photo' => 'photos/kyun.jpg',
-        ]);
-        Song::create([
-            'title' => 'Footsteps',
-            'release' => '2019-03-27',
-            'lyricist' => '秋元康',
-            'composer' => '野井洋児',
-            'arranger' => '野井洋児',
-            'is_recorded' => 'キュン',
-            'titlesong' => '0',
-            'youtube' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/VSQD18hsYz8?si=i05FermbXZ-e4C5l" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
-            'photo' => 'photos/kyun.jpg',
-        ]);
-        Song::create([
-            'title' => 'ときめき草',
-            'release' => '2019-03-27',
-            'lyricist' => '秋元康',
-            'composer' => '御子柴リョーマ',
-            'arranger' => '御子柴リョーマ',
-            'is_recorded' => 'キュン',
-            'titlesong' => '0',
-            'youtube' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/bgCR9HlsSPY?si=xWBqFwWkQVs1vFDn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
-            'photo' => 'photos/kyun.jpg',
-        ]);
-        Song::create([
-            'title' => '沈黙が愛なら',
-            'release' => '2019-03-27',
-            'lyricist' => '秋元康',
-            'composer' => 'サトウシンゴ',
-            'arranger' => 'サトウシンゴ、長田直也',
-            'is_recorded' => 'キュン',
-            'titlesong' => '0',
-            'youtube' => '-',
-            'photo' => 'photos/kyun.jpg',
+            'youtube' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/15Hsw9QSeoc?si=zB7PLxkarTZf4d8f" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+            'photo' => 'photos/bokunanka.jpg',
         ]);
     }
 }
