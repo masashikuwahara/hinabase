8d6b1f3759e1953c2d49c6b38e661ac01716c5f7
masashikuwahara <brucemclarenp4@gmail.com>
2025-02-21 00:47:19 +0900

    Seeder変更

--- database/seeders/SongMemberSeeder.php
+++ database/seeders/SongMemberSeeder.php
@@ -11,36 +11,42 @@ class SongMemberSeeder extends Seeder
 {
     public function run()
     {
-        $songA = Song::where('title', '僕なんか')->first();
-        $songB = Song::where('title', '飛行機雲ができる理由')->first();
-        $songC = Song::where('title', 'もうこんなに好きになれない')->first();
-        $songD = Song::where('title', 'ゴーフルと君')->first();
-        $songE = Song::where('title', '真夜中の懺悔大会')->first();
-        $songF = Song::where('title', '恋した魚は空を飛ぶ')->first();
-        $songG = Song::where('title', '知らないうちに愛されていた')->first();
+        $songA = Song::where('title', '絶対的第六感')->first();
+        $songB = Song::where('title', '君を覚えてない')->first();
+        $songC = Song::where('title', '永遠のソフィア')->first();
+        $songD = Song::where('title', 'どっちが先に言う?')->first();
+        $songE = Song::where('title', '妄想コスモス')->first();
+        $songF = Song::where('title', '雪は降る 心の世界に')->first();
+        $songG = Song::where('title', '夕陽Dance')->first();
 
-        $memberA = Member::where('name', '潮紗理菜')->first();
-        $memberB = Member::where('name', '影山優佳')->first();
-        $memberC = Member::where('name', '加藤史帆')->first();
-        $memberD = Member::where('name', '齊藤京子')->first();
-        $memberE = Member::where('name', '佐々木久美')->first();
-        $memberF = Member::where('name', '佐々木美玲')->first();
-        $memberG = Member::where('name', '高瀬愛奈')->first();
-        $memberH = Member::where('name', '高本彩花')->first();
-        $memberI = Member::where('name', '東村芽依')->first();
-        $memberJ = Member::where('name', '金村美玖')->first();
-        $memberK = Member::where('name', '河田陽菜')->first();
-        $memberL = Member::where('name', '小坂菜緒')->first();
-        $memberM = Member::where('name', '富田鈴花')->first();
-        $memberN = Member::where('name', '丹生明里')->first();
-        $memberO = Member::where('name', '濱岸ひより')->first();
-        $memberP = Member::where('name', '松田好花')->first();
-        $memberQ = Member::where('name', '宮田愛萌')->first();
-        $memberR = Member::where('name', '渡邉美穂')->first();
-        $memberS = Member::where('name', '上村ひなの')->first();
-        $memberT = Member::where('name', '髙橋未来虹')->first();
-        $memberU = Member::where('name', '森本茉莉')->first();
-        $memberV = Member::where('name', '山口陽世')->first();
+        $memberA = Member::where('name', '加藤史帆')->first();
+        $memberB = Member::where('name', '佐々木久美')->first();
+        $memberC = Member::where('name', '佐々木美玲')->first();
+        $memberD = Member::where('name', '東村芽依')->first();
+        $memberE = Member::where('name', '金村美玖')->first();
+        $memberF = Member::where('name', '河田陽菜')->first();
+        $memberG = Member::where('name', '小坂菜緒')->first();
+        $memberH = Member::where('name', '富田鈴花')->first();
+        $memberI = Member::where('name', '丹生明里')->first();
+        $memberJ = Member::where('name', '松田好花')->first();
+        $memberK = Member::where('name', '上村ひなの')->first();
+        $memberL = Member::where('name', '髙橋未来虹')->first();
+        $memberM = Member::where('name', '小西夏菜実')->first();
+        $memberN = Member::where('name', '正源司陽子')->first();
+        $memberO = Member::where('name', '藤嶌果歩')->first();
+
+        $member1 = Member::where('name', '高瀬愛奈')->first();
+        $member2 = Member::where('name', '濱岸ひより')->first();
+        $member3 = Member::where('name', '森本茉莉')->first();
+        $member4 = Member::where('name', '山口陽世')->first();
+        $member5 = Member::where('name', '石塚瑶季')->first();
+        $member6 = Member::where('name', '清水理央')->first();
+        $member7 = Member::where('name', '竹内希来里')->first();
+        $member8 = Member::where('name', '平尾帆夏')->first();
+        $member9 = Member::where('name', '平岡海月')->first();
+        $member10 = Member::where('name', '宮地すみれ')->first();
+        $member11 = Member::where('name', '山下葉留花')->first();
+        $member12 = Member::where('name', '渡辺莉奈')->first();
 
         $songA->members()->attach([
             $memberA->id => ['is_center' => false],
@@ -54,19 +60,26 @@ class SongMemberSeeder extends Seeder
             $memberI->id => ['is_center' => false],
             $memberJ->id => ['is_center' => false],
             $memberK->id => ['is_center' => false],
-            $memberL->id => ['is_center' => true],
+            $memberL->id => ['is_center' => false],
             $memberM->id => ['is_center' => false],
-            $memberN->id => ['is_center' => false],
-            $memberO->id => ['is_center' => false],
-            $memberP->id => ['is_center' => false],
-            $memberQ->id => ['is_center' => false],
-            $memberR->id => ['is_center' => false],
-            $memberS->id => ['is_center' => false],
-            $memberT->id => ['is_center' => false],
-            $memberU->id => ['is_center' => false],
-            $memberV->id => ['is_center' => false],
+            $memberN->id => ['is_center' => true],
+            $memberO->id => ['is_center' => true],
         ]);
         $songB->members()->attach([
+            $member1->id => ['is_center' => false],
+            $member2->id => ['is_center' => false],
+            $member3->id => ['is_center' => false],
+            $member4->id => ['is_center' => false],
+            $member5->id => ['is_center' => false],
+            $member6->id => ['is_center' => false],
+            $member7->id => ['is_center' => false],
+            $member8->id => ['is_center' => false],
+            $member9->id => ['is_center' => false],
+            $member10->id => ['is_center' => true],
+            $member11->id => ['is_center' => false],
+            $member12->id => ['is_center' => false],
+        ]);
+        $songC->members()->attach([
             $memberA->id => ['is_center' => false],
             $memberB->id => ['is_center' => false],
             $memberC->id => ['is_center' => false],
@@ -78,74 +91,44 @@ class SongMemberSeeder extends Seeder
             $memberI->id => ['is_center' => false],
             $memberJ->id => ['is_center' => false],
             $memberK->id => ['is_center' => false],
-            $memberL->id => ['is_center' => true],
-            $memberM->id => ['is_center' => false],
-            $memberN->id => ['is_center' => false],
-            $memberO->id => ['is_center' => false],
-            $memberP->id => ['is_center' => false],
-            $memberQ->id => ['is_center' => false],
-            $memberR->id => ['is_center' => false],
-            $memberS->id => ['is_center' => false],
-            $memberT->id => ['is_center' => false],
-            $memberU->id => ['is_center' => false],
-            $memberV->id => ['is_center' => false],
-        ]);
-        $songC->members()->attach([
-            $memberJ->id => ['is_center' => false],
             $memberL->id => ['is_center' => false],
-            $memberO->id => ['is_center' => false],
+            $memberM->id => ['is_center' => false],
+            $memberN->id => ['is_center' => true],
+            $memberO->id => ['is_center' => true],
         ]);
         $songD->members()->attach([
-            $memberS->id => ['is_center' => false],
-            $memberT->id => ['is_center' => false],
-            $memberU->id => ['is_center' => false],
-            $memberV->id => ['is_center' => true],
+            $memberA->id => ['is_center' => false],
         ]);
         $songE->members()->attach([
-            $memberA->id => ['is_center' => true],
-            $memberB->id => ['is_center' => false],
-            $memberC->id => ['is_center' => false],
             $memberD->id => ['is_center' => false],
-            $memberE->id => ['is_center' => false],
-            $memberF->id => ['is_center' => false],
-            $memberG->id => ['is_center' => false],
-            $memberH->id => ['is_center' => false],
             $memberI->id => ['is_center' => false],
         ]);
         $songF->members()->attach([
-            $memberJ->id => ['is_center' => false],
-            $memberK->id => ['is_center' => false],
-            $memberL->id => ['is_center' => false],
-            $memberM->id => ['is_center' => false],
-            $memberN->id => ['is_center' => false],
-            $memberO->id => ['is_center' => false],
-            $memberP->id => ['is_center' => false],
-            $memberQ->id => ['is_center' => false],
-            $memberR->id => ['is_center' => true],
+            $member1->id => ['is_center' => false],
+            $member2->id => ['is_center' => false],
+            $member3->id => ['is_center' => false],
+            $member4->id => ['is_center' => false],
+            $member5->id => ['is_center' => false],
+            $member6->id => ['is_center' => false],
+            $member7->id => ['is_center' => false],
+            $member8->id => ['is_center' => false],
+            $member9->id => ['is_center' => false],
+            $member10->id => ['is_center' => true],
+            $member11->id => ['is_center' => false],
+            $member12->id => ['is_center' => false],
         ]);
         $songG->members()->attach([
-            $memberA->id => ['is_center' => false],
-            $memberB->id => ['is_center' => false],
-            $memberC->id => ['is_center' => false],
-            $memberD->id => ['is_center' => false],
-            $memberE->id => ['is_center' => false],
-            $memberF->id => ['is_center' => false],
-            $memberG->id => ['is_center' => false],
-            $memberH->id => ['is_center' => false],
-            $memberI->id => ['is_center' => false],
-            $memberJ->id => ['is_center' => false],
-            $memberK->id => ['is_center' => false],
+            $member5->id => ['is_center' => false],
             $memberL->id => ['is_center' => false],
-            $memberM->id => ['is_center' => false],
+            $member6->id => ['is_center' => false],
             $memberN->id => ['is_center' => false],
+            $member7->id => ['is_center' => false],
+            $member8->id => ['is_center' => false],
+            $member9->id => ['is_center' => false],
             $memberO->id => ['is_center' => false],
-            $memberP->id => ['is_center' => false],
-            $memberQ->id => ['is_center' => false],
-            $memberR->id => ['is_center' => false],
-            $memberS->id => ['is_center' => false],
-            $memberT->id => ['is_center' => false],
-            $memberU->id => ['is_center' => false],
-            $memberV->id => ['is_center' => false],
+            $member10->id => ['is_center' => false],
+            $member11->id => ['is_center' => false],
+            $member12->id => ['is_center' => true],
         ]);
     }
 }
--- database/seeders/SongSeeder.php
+++ database/seeders/SongSeeder.php
@@ -11,15 +11,15 @@ class SongSeeder extends Seeder
     public function run()
     {
         Song::create([
-            'title' => '僕なんか',
-            'release' => '2022-06-01',
+            'title' => '絶対的第六感',
+            'release' => '2024-09-18',
             'lyricist' => '秋元康',
-            'composer' => '温詞',
-            'arranger' => '温詞、TomoLow',
-            'is_recorded' => '僕なんか',
+            'composer' => 'シライシ紗トリ',
+            'arranger' => 'シライシ紗トリ',
+            'is_recorded' => '絶対的第六感',
             'titlesong' => '1',
-            'youtube' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/15Hsw9QSeoc?si=zB7PLxkarTZf4d8f" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
-            'photo' => 'photos/bokunanka.jpg',
+            'youtube' => '<iframe width="560" height="315" src="https://www.youtube.com/embed/qUZagu-NL_s?si=bdKF6OMh3QdT58YI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+            'photo' => 'photos/zettaiteki.jpg',
         ]);
     }
 }
