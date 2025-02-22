540865697f90a2087292ac031dcf562e62bd1de4
masashikuwahara <brucemclarenp4@gmail.com>
2025-02-23 01:32:51 +0900

    foooter共通化

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="bg-[#7cc7e8] text-white text-center py-4 mt-8">
+  <p class="text-sm">&copy; {{ date('Y') }} HINABASE. All rights reserved.</p>
+  <div class="flex justify-center space-x-4 mt-2">
+      <a href="#" class="hover:underline">Twitter</a>
+      <a href="#" class="hover:underline">Instagram</a>
+      <a href="#" class="hover:underline">YouTube</a>
+  </div>
+</footer>
