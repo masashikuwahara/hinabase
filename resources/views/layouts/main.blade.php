489ffaa39e3d8112c865559be31b4a4b79408a79
masashikuwahara <brucemclarenp4@gmail.com>
2025-02-24 04:42:38 +0900

    meta情報追加

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+    <meta name="description" content="日向坂46のシンプルなデータベースサイトです。メンバー情報、楽曲情報を載せています。">
+    <meta name="format-detection" content="email=no,telephone=no,address=no">
+    <meta property="og:title" content="HINABASE">
+    <meta property="og:description" content="日向坂46のシンプルなデータベースサイトです。メンバー情報、楽曲情報を載せています。">
+    <meta property="og:image" content="https://kasumizaka46.com/storage/images/logo.png">
+    <meta property="og:url" content="https://kasumizaka46.com"> 
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="HINABASE">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
