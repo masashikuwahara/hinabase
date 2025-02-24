4155b786bb4827e2f88bdfdaeb33ab8ad096f6b0
masashikuwahara <brucemclarenp4@gmail.com>
2025-02-25 00:47:31 +0900

    alpinejs実装

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,6 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     @vite('resources/css/app.css')
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <style>
         .youtube-ratio iframe {
             width: 100%;
