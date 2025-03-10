e17c8190eff756209ea009ff9114c059c9d3b3c8
masashikuwahara <brucemclarenp4@gmail.com>
2025-03-11 02:03:28 +0900

    membersテーブルにsnsカラム追加

--- database/migrations/2025_03_10_060027_add_sns_to_members_table.php
+++ database/migrations/2025_03_10_060027_add_sns_to_members_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('members', function (Blueprint $table) {
-            //
+            $table->string('sns')->nullable()->after('colorname2');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('members', function (Blueprint $table) {
-            //
+            $table->dropColumn('sns');
         });
     }
 };
