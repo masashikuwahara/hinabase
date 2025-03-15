0466a1e70d047014fb99a6a04cdbc4f8eac1eaa8
masashikuwahara <brucemclarenp4@gmail.com>
2025-03-15 15:07:54 +0900

    スクレイピング機能実装

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Member;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+use Illuminate\Support\Facades\View; 
 
 class MemberController extends Controller
 {
@@ -17,6 +20,26 @@ class MemberController extends Controller
     public function show($id)
     {
         $member = Member::with('songs')->findOrFail($id); // メンバー情報と参加楽曲を取得
-        return view('members.show', compact('member'));
+        
+        if ($member->blog_url) {
+            $client = new Client();
+            try {
+                $response = $client->request('GET', $member->blog_url);
+                $html = $response->getBody()->getContents();
+    
+                $crawler = new Crawler($html);
+                $title = $crawler->filter('.c-blog-article__title')->text();
+                $content = $crawler->filter('.c-blog-article__text')->text();
+                $time = $crawler->filter('.c-blog-article__date')->text();
+    
+                $blogHtml = "<a href='{$member->blog_url}' target='_blank' rel='noopener noreferrer'>{$time}&nbsp;{$title}</a>";
+            } catch (\Exception $e) {
+                $blogHtml = 'ブログ情報の取得に失敗しました。';
+            }
+        } else {
+            $blogHtml = 'ブログがありません。';
+        }
+
+        return view('members.show', compact('member', 'blogHtml'));
     }
 }
