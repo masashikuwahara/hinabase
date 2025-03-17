547551ab21414e72a039b35aabc10bd68ec550e4
masashikuwahara <brucemclarenp4@gmail.com>
2025-03-18 00:13:57 +0900

    メタ情報追加

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+    <meta name="keyword" content="日向坂46,ヒナタザカ46,ひなたざか46,hinatazaka46,潮紗理菜,加藤史帆,齊藤京子,佐々木久美,
+    佐々木美玲,高瀬愛奈,高本彩花,東村芽依,金村美玖,河田陽菜,小坂菜緒,富田鈴花,丹生明里,濱岸ひより,松田好花,上村ひなの,髙橋未来虹,
+    森本茉莉,山口陽世,石塚瑶季,小西夏菜実,清水理央,正源司陽子,竹内希来里,平尾帆夏,平岡海月,藤嶌果歩,宮地すみれ,山下葉留花,渡辺莉奈" />
     <meta name="description" content="日向坂46のシンプルなデータベースサイトです。メンバー情報、楽曲情報を載せています。">
     <meta name="format-detection" content="email=no,telephone=no,address=no">
     <meta property="og:title" content="HINABASE">
@@ -33,4 +36,3 @@
 
 </body>
 </html>
-<!-- version1.1.0 -->
