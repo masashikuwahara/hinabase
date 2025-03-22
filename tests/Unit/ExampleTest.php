97d1ea5f559c8f6c4dd37439f5d8e69194a3c029
masashikuwahara <brucemclarenp4@gmail.com>
2025-03-23 01:18:34 +0900

    テストケース追加

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,5 +1,11 @@
 <?php
 
-test('that true is true', function () {
-    expect(true)->toBeTrue();
-});
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertEquals(2, 1 + 1); // 1+1は2なので、テストが成功する
+    }
+}
