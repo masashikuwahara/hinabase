de9e5f4d1e6fff521443811c124ddcc9c24e9f33
masashikuwahara <brucemclarenp4@gmail.com>
2025-03-30 23:13:06 +0900

    フォーム修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="text-center mt-8">
-        <p class="text-sm">日向坂46のメンバーと楽曲のシンプルなデータベースサイトです。</p>
+        <p class="text-base">日向坂46のメンバーと楽曲のシンプルなデータベースサイトです。</p>
     </div>
 
     <!-- 検索フォーム -->
@@ -13,7 +13,7 @@
             <div class="text-red-600 font-semibold mb-2">{{ session('error') }}</div>
         @endif
         <form action="{{ route('search') }}" method="GET" class="inline-block bg-white p-3 rounded shadow-md">
-            <input type="text" name="query" placeholder="メンバー名、楽曲名で検索" 
+            <input type="text" name="query" placeholder="小坂菜緒 天才 キュン" 
                 class="border p-2 rounded-l-md focus:outline-none focus:ring focus:ring-[#7cc7e8]">
             <button type="submit" class="bg-[#7cc7e8] text-white p-2 rounded-r-md hover:bg-[#5aa6c4]">
                 検索
@@ -23,6 +23,7 @@
     <div class="text-center mt-8">
         <p class="text-sm">メンバー名、あだ名、楽曲名で検索できます。</p>
     </div>
+    
     <!-- メンバー一覧 -->
     <section class="mt-10 px-6">
         <h2 class="text-xl font-bold mb-4">メンバー</h2>
@@ -71,12 +72,18 @@
 
     <div class="text-center mt-8">
         <p class="text-sm">--更新履歴--</p>
+    </div>
+    <div class="text-center w-100 max-h-40 overflow-y-scroll bg-white p-4 rounded-lg shadow-md">
         <div class="text-left inline-block">
-            <p class="text-sm">2025.03.22&nbsp;v1.4.0&nbsp;メンバー一覧ページにソート機能追加</p>
-            <p class="text-sm">2025.03.15&nbsp;v1.3.0&nbsp;メンバー詳細ページにブログ情報追加</p>
-            <p class="text-sm">2025.03.14&nbsp;v1.2.0&nbsp;検索システム改修、ニックネーム検索に対応</p>
-            <p class="text-sm">2025.03.09&nbsp;v1.1.0&nbsp;メンバー詳細ページにSNS情報追加</p>
-            <p class="text-sm">2025.03.01&nbsp;v1.0.0&nbsp;リリース</p>
+            <p class="text-sm">2025.03.31&nbsp;五期生メンバーを追加</p>
+            <p class="text-sm">2025.03.28&nbsp;v.1.6.1&nbsp;メンバー詳細ページにスキル表追加</p>
+            <p class="text-sm">2025.03.26&nbsp;v.1.5.0&nbsp;楽曲詳細ページに歌詞リンク追加</p>
+            <p class="text-sm">2025.03.22&nbsp;v.1.4.0&nbsp;メンバー一覧ページにソート機能追加</p>
+            <p class="text-sm">2025.03.15&nbsp;v.1.3.0&nbsp;メンバー詳細ページにブログ情報追加</p>
+            <p class="text-sm">2025.03.14&nbsp;v.1.2.0&nbsp;検索システム改修、あだ名検索に対応</p>
+            <p class="text-sm">2025.03.09&nbsp;v.1.1.0&nbsp;メンバー詳細ページにSNS情報追加</p>
+            <p class="text-sm">2025.03.01&nbsp;v.1.0.0&nbsp;リリース</p>
         </div>
     </div>
+{{-- v.1.6.1 --}}
 @endsection
