e6b8dd17fbf0475d4b19c67e898c14a93926cfdc
test <brucemclarenp4@gmail.com>
2025-09-05 01:13:18 +0900

    削除

--- app/Console/Commands/GenerateSitemap.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$sitemap->writeToFile(public_path('sitemap.xml'));
--- app/Console/Kernel.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-protected function schedule(Schedule $schedule): void
-{
-    $schedule->command('site:generate-sitemap')->daily(); // 毎日生成
-}
