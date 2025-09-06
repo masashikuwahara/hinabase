3760d7297c90f761a37bd86b3a5ea5e8f832e7a8
test <brucemclarenp4@gmail.com>
2025-09-06 17:24:11 +0900

    songモデル修正

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Song extends Model
 {
@@ -16,5 +17,10 @@ class Song extends Model
     {
         return $this->belongsToMany(Member::class, 'song_members')
         ->withPivot('is_center');
+    }
+
+    public function getIsRecentlyUpdatedAttribute()
+    {
+        return $this->updated_at->gt(Carbon::now()->subDays(10));
     }
 }
