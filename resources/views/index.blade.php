b5cb4572e61bef5e912149ccdd5031d1e9cfd36b
test <brucemclarenp4@gmail.com>
2025-10-01 22:17:18 +0900

    indexにリンク追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -122,7 +122,9 @@
             @endforelse
         </div>
     </div>
-
+    <div class="mt-6 text-center">
+        <a href="{{ route('popular.index') }}" class="text-sm text-gray-600 hover:text-gray-800 underline">人気ページTOP12</a>
+    </div>
     <!-- 閲覧履歴の処理  -->
     <script>
     (() => {
@@ -157,5 +159,5 @@
     });
     })();
     </script>
-<!-- v.1.23.0 -->
+<!-- v.1.24.1 -->
 @endsection
