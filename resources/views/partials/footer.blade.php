6e625946bcdcec2f80dcc0f24c30fa1906fae1dc
test <brucemclarenp4@gmail.com>
2025-10-24 00:03:59 +0900

    フッターリンク追加

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,9 +1,17 @@
 <footer class="bg-[#7cc7e8] text-white text-center py-4 mt-8">
-  <p class="text-sm">&copy; {{ date('Y') }} HINABASE. All rights reserved.</p>
-  <div class="flex justify-center space-x-4 mt-2">
-      <a href="https://x.com/sakamichiiwlu4e" target="_blank" rel="noopener noreferrer" class="hover:underline">X</a>
-      <a href="https://kasumizaka46.xsrv.jp/" target="_blank" rel="noopener noreferrer" class="hover:underline">SAKURAAC</a>
-      {{-- <a href="#" class="hover:underline">Instagram</a> --}}
-      {{-- <a href="#" class="hover:underline">YouTube</a> --}}
+  <p class="text-xs sm:text-sm">&copy; {{ date('Y') }} HINABASE. All rights reserved.</p>
+
+  <div class="flex flex-wrap justify-center items-center mt-3 text-xs sm:text-sm">
+    <a href="{{ route('members.index') }}" class="mx-2 hover:underline">メンバー一覧</a>
+    <span class="text-white/70">|</span>
+    <a href="{{ route('songs.index') }}" class="mx-2 hover:underline">楽曲一覧</a>
+    <span class="text-white/70">|</span>
+    <a href="{{ route('popular.index') }}" class="mx-2 hover:underline">人気ページTOP20</a>
+    <span class="text-white/70">|</span>
+    <a href="{{ route('others.index') }}" class="mx-2 hover:underline">その他</a>
+    <span class="text-white/70">|</span>
+    <a href="https://x.com/sakamichiiwlu4e" target="_blank" rel="noopener noreferrer" class="mx-2 hover:underline">X</a>
+    <span class="text-white/70">|</span>
+    <a href="https://x.gd/I7a73" target="_blank" rel="noopener noreferrer" class="mx-2 hover:underline">Instagram</a>
   </div>
 </footer>
