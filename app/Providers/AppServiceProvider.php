f5928bf840e335f45432803b49910c618c2fa3ed
test <brucemclarenp4@gmail.com>
2025-11-10 18:20:53 +0900

    ページ制限

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,9 @@
 namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Support\Facades\RateLimiter;
-
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,21 +20,52 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+    // public function boot()
+    // {
+    //     RateLimiter::for('songs-limit', function ($request) {
+    //         $ip = $request->ip();
+    //         $songId = $request->route('id');
+
+    //         if ($songId == 150) {
+    //             return [
+    //                 Limit::perMinute(1,1440)->by($ip),
+    //             ];
+    //         }
+
+    //         return [
+    //             Limit::perMinute(30)->by($ip),
+    //         ];
+    //     });
+    // }
+
     public function boot()
     {
         RateLimiter::for('songs-limit', function ($request) {
-            $ip = $request->ip();
             $songId = $request->route('id');
+                Log::info('RateLimiter executed', [
+        'songId' => $songId,
+        'ip' => $request->ip(),
+    ]);
 
-            if ($songId == 150) {
-                return [
-                    Limit::perMinute(1,1440)->by($ip),
-                ];
-            }
+    if ($songId == 150) {
+        $key = 'song150_access_' . now()->toDateString();
+        $count = Cache::get($key, 0);
+        $limit = 5;
 
-            return [
-                Limit::perMinute(30)->by($ip),
-            ];
-        });
+        if ($count >= $limit) {
+            return Limit::perMinute(1)->by('global')->response(function () {
+                abort(429);
+            });
+        }
+
+        $expiresAt = now()->addDay()->startOfDay();
+        Cache::put($key, $count + 1, $expiresAt);
+        return [Limit::perMinute(30)];
     }
+
+    return [
+        Limit::perMinute(30)->by($request->ip()),
+    ];
+    });
+  }
 }
