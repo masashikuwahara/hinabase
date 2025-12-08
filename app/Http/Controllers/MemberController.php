b16e8936beaa08f7fd0b0d98614207f6e1651d53
test <brucemclarenp4@gmail.com>
2025-12-08 19:36:43 +0900

    不要箇所削除

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -67,49 +67,6 @@ class MemberController extends Controller
 
         return view('members.index', compact('currentMembers', 'graduatedMembers', 'sort', 'order'));
     }
-    // public function show($id)
-    // {
-    //     $member = Member::with('songs')->findOrFail($id); // メンバー情報と参加楽曲を取得
-    //     $songCount = $member->songs->count();
-    //     $centerCount = $member->songs->where('pivot.is_center', true)->count(); // センター回数を取得
-    //     $titlesongCount = $member->songs->where('titlesong', 1)->count(); // 選抜回数を取得
-    //     $radar = Member::with('skill')->find($id);
-        
-    //     // レーダーチャート用データ（例: 各スキル 100 点満点）
-    //     $radarData = [
-    //         'singing' => $radar->singing,
-    //         'dancing' => $radar->dancing,
-    //         'variety' => $radar->variety,
-    //         'intelligence' => $radar->intelligence,
-    //         'sport' => $radar->sport,
-    //         'burikko' => $radar->burikko,
-    //     ];
-        
-    //     if ($radar === null) {
-    //         $radar = 50;
-    //     };
-
-    //     if ($member->blog_url) {
-    //         $client = new Client();
-    //         try {
-    //             $response = $client->request('GET', $member->blog_url);
-    //             $html = $response->getBody()->getContents();
-    
-    //             $crawler = new Crawler($html);
-    //             $title = $crawler->filter('.c-blog-article__title')->text();
-    //             $content = $crawler->filter('.c-blog-article__text')->text();
-    //             $time = $crawler->filter('.c-blog-article__date')->text();
-    
-    //             $blogHtml = "<a href='{$member->blog_url}' target='_blank' rel='noopener noreferrer'>{$time}&nbsp;{$title}</a>";
-    //         } catch (\Exception $e) {
-    //             $blogHtml = 'ブログ情報の取得に失敗しました。';
-    //         }
-    //     } else {
-    //         $blogHtml = 'ブログは終了しました。';
-    //     }
-
-    //     return view('members.show', compact('member',  'centerCount','titlesongCount', 'songCount', 'blogHtml', 'radarData', 'radar'));
-    // }
 
     public function show($id)
     {
