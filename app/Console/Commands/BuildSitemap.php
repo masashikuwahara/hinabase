c8d855af7cffd05632771d3ff0441329fc346364
test <brucemclarenp4@gmail.com>
2026-03-07 04:14:38 +0900

    builtsitemap追加

--- app/Console/Commands/BuildSitemap.php
+++ app/Console/Commands/BuildSitemap.php
@@ -24,6 +24,7 @@ class BuildSitemap extends Command
         $urls[] = ['loc' => url('/youtube/ranking'), 'lastmod' => now()->toAtomString()];
         $urls[] = ['loc' => url('/timeline'), 'lastmod' => now()->toAtomString()];
         $urls[] = ['loc' => url('/members/stats'), 'lastmod' => now()->toAtomString()];
+        $urls[] = ['loc' => url('/graphs/hinata-relationship-5th'), 'lastmod' => now()->toAtomString()];
 
         foreach (\App\Models\Member::cursor() as $m) {
             $urls[] = [
